98a1d28daabb5d477e80393d609c058a5ca6ccae
iExoTicK <alexis59680@hotmail.fr>
2019-10-21 11:44:56 +0200

    Exo Année Bissextile

---
+++ Exo-Annee.py
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/env python3
+
+year = int(input('Ecrire la date : '))
+
+if (year %4 == 0 and year %100 != 0  or year %400 == 0):
+	print("C'est une année Bissextile")
+else:
+	print("Ce n'est pas une année Bissextile")
