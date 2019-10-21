3c9d5e84aafb070b7b537173e7808d4b9a0bb898
Alexis <alexis@formation.poe>
2019-10-21 10:38:25 +0200

    script

---
+++ exo.py
@@ -0,0 +1,12 @@
+import random
+
+animaux=["le saumon","le rat","le loup","la belette","le singe"]
+prenom=["roger","arezki","rei","aya","vincent"]
+
+index1 = random.randint(0, len(animaux)-1)
+index2 = random.randint(0, len(prenom)-1)
+
+#rdm=prenom[index2] + " " + animaux[index1]
+#print(rdm)
+
+print(prenom[index2] + " " + animaux[index1])
